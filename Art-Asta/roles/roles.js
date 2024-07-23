49c2833c2b56c81790ae8c5f4cba8e13227ee3c0
Prabh Simran Singh <67994234+imprabhsimran@users.noreply.github.com>
2024-07-23 15:01:12 -0700

    Add files via upload

---
+++ Art-Asta/roles/roles.js
@@ -0,0 +1,81 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, doc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import firebaseConfig from '../firebaseConfig.js';
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const artistDiv = document.getElementById('artist');
+    const artistEnthusiastDiv = document.getElementById('art-enthusiast');
+
+
+    // Function to update user role in Firestore
+    async function updateUserRoleArtist(role) {
+        try {
+            const user = auth.currentUser;
+
+            if (user) {
+                const userId = user.uid;
+
+                await updateDoc(doc(db, 'Users', userId), {
+                    role: role
+                });
+
+                window.location.href = '/Artist-info/artist-info-main-page.html';
+            } else {
+                console.error('No user is currently signed in.');
+                alert('Please sign in again.');
+            }
+        } catch (error) {
+            console.error('Error updating role:', error);
+            alert('Error updating role.');
+        }
+    }
+
+    async function updateUserRoleArtEnthusiast(role) {
+        try {
+            const user = auth.currentUser;
+
+            if (user) {
+                const userId = user.uid;
+
+                await updateDoc(doc(db, 'Users', userId), {
+                    role: role
+                });
+
+                window.location.href = '/Art-enthusiast/art-enthusiast.html';
+            } else {
+                console.error('No user is currently signed in.');
+                alert('Please sign in again.');
+                window.location.href = '/Sign-up/Sign-up.html';
+            }
+        } catch (error) {
+            console.error('Error updating role:', error);
+            alert('Error updating role.');
+        }
+    }
+
+    // Ensure user is signed in
+    auth.onAuthStateChanged((user) => {
+        if (user) {
+            console.log(user);
+            // Event listeners for role selection
+            artistDiv.addEventListener('click', function() {
+                updateUserRoleArtist('Artist');
+            });
+
+            artistEnthusiastDiv.addEventListener('click', function() {
+                updateUserRoleArtEnthusiast('Art Enthusiast');
+            });
+        } else {
+            console.error('No user is currently signed in.');
+            alert('Please sign in.');
+            window.location.href = '/Sign-up/Sign-up.html';
+        }
+
+    });
+});
