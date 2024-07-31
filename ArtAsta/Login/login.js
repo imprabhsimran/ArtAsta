b0d840d9cea101ed479ce0c8e30767db57efe665
Prabh Simran Singh <67994234+imprabhsimran@users.noreply.github.com>
2024-07-30 19:27:50 -0700

    Update login.js

--- ArtAsta/Login/login.js
+++ ArtAsta/Login/login.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import {firebaseConfig} from "../firebaseConfig.js";
+import {firebaseConfig} from "./firebaseConfig.js";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
